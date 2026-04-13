55e69bb8402d83262d0225d20c5545a26a149a57
einarsejje-666 <einar.sejje@tecsup.edu.pe>
2026-04-13 14:27:24 -0500

    feat: upgrade style of Greeting component

--- app/src/main/java/com/example/lab_04/Greeting.kt
+++ app/src/main/java/com/example/lab_04/Greeting.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -12,8 +13,9 @@ import androidx.compose.ui.unit.sp
 fun Greeting(name: String = "Student") {
     Text(
         text = "Hello, $name! 👋",
-        fontSize = 24.sp,
+        fontSize = 32.sp,
         fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(16.dp)
+        color = Color.Blue,                    // ← ahora sí funciona
+        modifier = Modifier.padding(24.dp)
     )
 }
